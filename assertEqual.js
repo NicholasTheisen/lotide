75a04821f753dcb518c99f03ef65c682d6e26502
NicholasTheisen <nicholas.theisen1997@gmail.com>
2023-11-16 05:50:49 +0000

    Assert Equal Function

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed:", actual, "===", expected);
+  } else {
+    console.log("❌❌❌ Assertion Failed:", actual, "!==", expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(5, 5);
+assertEqual("Nick", "Nicholas");
+assertEqual("Ice Cream", "Ice Cream");
