c8dfe2da7656a3874ce6f64c08e02e437936f1c6
NicholasTheisen <nicholas.theisen1997@gmail.com>
2023-11-19 04:53:17 +0000

    Implementation of assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrayEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays([1], [1]), true);
+assertEqual(eqArrays([1], []), false);
