dddf88412ce1f1d1ec4394e164704c3f393063c0
NicholasTheisen <nicholas.theisen1997@gmail.com>
2023-11-19 04:35:13 +0000

    Implementation of eqArrays.js Updated**

--- eqArrays.js
+++ eqArrays.js
@@ -22,3 +22,6 @@ const eqArrays = function (arr1, arr2) {
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays([1], [1]), true);
+assertEqual(eqArrays([1], []), false);
