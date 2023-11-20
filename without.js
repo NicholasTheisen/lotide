cc6d9f9860802f0a36f48ffddae8bb36da79cd4f
NicholasTheisen <nicholas.theisen1997@gmail.com>
2023-11-20 02:01:54 +0000

    Implementation of the without.js file Updated

--- without.js
+++ without.js
@@ -32,11 +32,6 @@ const assertArrayEqual = function(actual, expected) {
 };
 
 
-assertArrayEqual(without([1, 2, 3,], [1]));
-assertArrayEqual(without(["a", "b", "c", "d"], ['a', 'c']));
-assertArrayEqual(without(["1", "2", "3"], [1, 2, "3"]));
-assertArrayEqual(without([1, 3, 5, 7, 9], [1, 2, 3]));
-
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
