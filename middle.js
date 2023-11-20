8a5e859212534ea7f10581a0a2a0a6385e37d80c
NicholasTheisen <nicholas.theisen1997@gmail.com>
2023-11-20 02:59:02 +0000

    Implementation of middle.js

---
+++ middle.js
@@ -0,0 +1,39 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrayEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const middle = function(array) {
+  const length = array.length;
+  const middleIndex = Math.floor(length / 2);
+
+  if (length <= 2) {
+    return [];
+  } else if (length % 2 === 1) {
+    return [array[middleIndex]];
+  } else {
+    return [array[middleIndex - 1], array[middleIndex]];
+  }
+};
+
+// Test Cases
+assertArrayEqual(middle([1, 2, 3]), [2]);
+assertArrayEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArrayEqual(middle([1]), []);
+assertArrayEqual(middle([1, 2]), []);
