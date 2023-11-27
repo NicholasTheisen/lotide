d487d4deb7debc33f694c918674dc4485a4d686d
NicholasTheisen <nicholas.theisen1997@gmail.com>
2023-11-27 05:50:56 +0000

    Updated takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -32,11 +32,8 @@ const takeUntil = function(array, callback) {
 };
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+assertArrayEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]); // => should PASS
 
-console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+assertArrayEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]);
