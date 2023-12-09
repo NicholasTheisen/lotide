54eab8b73c1bee1452a1dab1d218a90762c906a6
NicholasTheisen <nicholas.theisen1997@gmail.com>
2023-12-09 21:22:32 +0000

    Lotide Refactor 4

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArrayEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -20,8 +8,5 @@ const assertArrayEqual = function(actual, expected) {
   }
 };
 
-assertArrayEqual([1, 2, 3], [1, 2, 3]);
-assertArrayEqual([1, 2, 3], [3, 2, 1]);
-assertArrayEqual([], []);
-assertArrayEqual([1], [1]);
-assertArrayEqual([1], []);
+module.exports = assertArrayEqual;
+
