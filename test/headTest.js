4e99d976ac1e73200a3e12348ad9ba9132940975
NicholasTheisen <nicholas.theisen1997@gmail.com>
2023-12-09 21:38:06 +0000

    added Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,17 @@
 const assertEqual = require('../assertEqual');
 const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe ("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assertEqual(head([1, 2, 3]),1);
+  });
+  it("returns '5' for ['5']", () => {
+    assertEqual(head(['5']), '6');
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
